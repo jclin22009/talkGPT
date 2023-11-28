78ba6da95291dd086c4b6b50b29ce8b04381ba2d
Jason Lin <jclin2.2009@gmail.com>
2023-11-28 11:34:00 -0800

    DOESNT WORK (streaming first pass)

--- main.py
+++ main.py
@@ -4,104 +4,122 @@ import speech_recognition as sr
 import elevenlabs
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-conversation=[{'role': 'system', 'content': """You are Jason Lin, a college student answering his door. Respond to folks knocking on your door. Be concise, witty, and natural. Here is some info about you:
+conversation = [
+    {
+        "role": "system",
+        "content": """You are Jason Lin, a college student answering his door. Respond to folks knocking on your door. Be concise, witty, and natural. Here is some info about you:
                
-               You live in the dorm Toyon. You study computer science at Stanford. You love to tinker. You made a motorized driving couch, and there is a robot arm in your room. You love to take photos.
+               You live in the dorm Toyon. You study computer science at Stanford. You love to build things. You made a motorized driving couch, and there is a robot arm in your room. You love to take photos. You like to drink tea.
                For most people coming to the room, just mention that you're currently busy but would love to chat later.
-               """}]
+               """,
+    }
+]
+
 
 def transcribe_speech(language: str) -> str:
     """
-        Records audio from the microphone and transcribes it into text.
+    Records audio from the microphone and transcribes it into text.
 
-        Args:
-            language: A string indicating the language of the speech to be transcribed.
+    Args:
+        language: A string indicating the language of the speech to be transcribed.
 
-        Returns:
-            A string containing the transcribed text.
+    Returns:
+        A string containing the transcribed text.
 
-        Raises:
-            sr.RequestError: If there is an error with the API request.
-            sr.UnknownValueError: If the speech could not be transcribed.
+    Raises:
+        sr.RequestError: If there is an error with the API request.
+        sr.UnknownValueError: If the speech could not be transcribed.
     """
-  
+
     print("\n\nListening...")
-    
+
     recognizer = sr.Recognizer()
     mic = sr.Microphone()
-    
+
     with mic as source:
-        
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
-        
+
         print("Recognizing...")
-        transcript = recognizer.recognize_whisper(audio, language=language, model="base.en") # local whisper
+        transcript = recognizer.recognize_whisper(
+            audio, language=language, model="base.en"
+        )  # local whisper
         # transcript = client.audio.transcriptions.create(file=audio.get_wav_data(), model="whisper-1") # OAI whisper
-        
+
         print(transcript)
         return transcript
 
 
 def send_request(language: str, words: str) -> None:
     """
-        Sends a request to the OpenAI API and speaks out the response.
+    Sends a request to the OpenAI API and speaks out the response.
 
-        Args:
-            language: A string indicating the language of the chat message.
-            words: A string containing the chat message to be sent.
+    Args:
+        language: A string indicating the language of the chat message.
+        words: A string containing the chat message to be sent.
 
-        Returns:
-            None.
+    Returns:
+        None.
 
-        Raises:
-            openai.error.OpenAIError: If there is an error with the OpenAI API request.
+    Raises:
+        openai.error.OpenAIError: If there is an error with the OpenAI API request.
     """
-    if words == "" or words == None or not any(c.isalpha() for c in words): # if no words were said
+    if (
+        words == "" or words == None or not any(c.isalpha() for c in words)
+    ):  # if no words were said
         print("No words were said.")
         return
-    completion = client.chat.completions.create(model="gpt-4-1106-preview",
-    messages = conversation + [
-      {
-          
-          "role": "user", 
-          "content": words
-      }
-    ], # stream=True at some point
-    )
+
     conversation.append({"role": "user", "content": words})
-    answer = completion.choices[0].message.content
-    conversation.append({"role": "assistant", "content": answer})
-    gen_audio(answer)
-    print(f'conversation so far: {conversation}')
-    
-def gen_audio(words: str) -> None:
+
+    # gpt_stream = get_gpt_stream()
+    # print("gpt_stream: ", gpt_stream)
+    gen_audio()
+
+    # conversation.append({"role": "assistant", "content": answer})
+    print(f"conversation so far: {conversation}")
+
+
+def get_gpt_stream():
     """
-        Generates audio from a string of text.
+    Returns a stream of GPT-3 responses. Is a generator.
+    """
+    completion = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=conversation,
+        stream=True,
+    )
 
-        Args:
-            words: A string containing the text to be converted to audio.
+    for response in completion:
+        # Process each chunk of response
+        print("resp:", response.choices[0].delta.content)
+        yield response.choices[0].delta.content
 
-        Returns:
-            None.
 
-        Raises:
-            openai.error.OpenAIError: If there is an error with the OpenAI API request.
+def gen_audio() -> None:
+    """
+    Generates audio from a string of text.
+
+    Args:
+        words: A string containing the text to be converted to audio.
+
+    Returns:
+        None.
+
+    Raises:
+        openai.error.OpenAIError: If there is an error with the OpenAI API request.
     """
     print("Generating audio...")
     audio = elevenlabs.generate(
-        text=words,
-        voice="Nicole", # for now
-        # model="eleven_monolingual_v1",
-        # stream=True,
+        text=get_gpt_stream(), voice="Nicole", stream=True  # for now
     )
-    # stream(audio)
     print("audio generated")
-    elevenlabs.play(audio)
+    elevenlabs.stream(audio)
+    # elevenlabs.play(audio)
+
 
 if __name__ == "__main__":
-    
-    language = 'english'
+    language = "english"
 
     while True:
         input_words = transcribe_speech(language)
